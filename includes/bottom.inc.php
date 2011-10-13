f00733affd7ec4e96c810016f56a0dbac4b8ab3a
Mike Mazzantini <physicsmazz@gmail.com>
2011-10-13 15:51:52 -0400

    Initial HTML and CSS

---
+++ includes/bottom.inc.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER['HTTP_HOST'] == 'localhost' || strstr($_SERVER['HTTP_HOST'],'192.168.2') || $_SERVER['HTTP_HOST'] == '127.0.0.1'){
+    echo ('<script src="js/libs/jquery-1.6.4.min.js">');
+}else{
+    echo<<<HEREDOC
+    <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.4.min.js"><\/script>')</script>
+HEREDOC;
+}
+?>
+<!-- scripts concatenated and minified via build script -->
+<script defer src="js/plugins.js"></script>
+<script defer src="js/script.js"></script>
+<!-- end scripts -->
+<?php
+if ($_SERVER['HTTP_HOST'] == 'localhost' || strstr($_SERVER['HTTP_HOST'],'192.168.2') || $_SERVER['HTTP_HOST'] == '127.0.0.1'){
+}else{
+    echo<<<HEREDOC
+    <!--<script>-->
+    <!--  var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview'],['_trackPageLoadTime']];-->
+    <!--  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];-->
+    <!--  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';-->
+    <!--  s.parentNode.insertBefore(g,s)}(document,'script'));-->
+    <!--</script>-->
+HEREDOC;
+}
+?>
+<!--[if lt IE 7 ]>
+  <script defer src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+  <script defer>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+<![endif]-->
+
+ 
---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>Restorations Inc</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body>
+  <div id="container">
+      <header>
+          <nav>
+              <ul>
+                  <li><a class="active" href="">HOME</a></li>
+                  <li><a href="">ABOUT US</a></li>
+                  <li><a href="">PORTFOLIO</a></li>
+                  <li><a href="">CONTACT US</a></li>
+              </ul>
+          </nav>
+          <h4 id="phoneNumber" class="visuallyhidden">413.655.0100</h4>
+          <h1 id="logo" class="ir">Restorations Inc.</h1>
+          <h2 class="visuallyhidden">GENERAL CONTRACTOR</h2>
+          <h4 id="address" class="visuallyhidden">151 South St. Hinsdale, Ma 01235</h4>
+      </header>
+      <h3 class="visuallyhidden">Taylor Development LLC</h3>
+      <div id="headerBottom"></div>
+      <div id="main" role="main">
+          <div id="slider">
+              <ul>
+                  <li><img src="img/slider/slider1.jpg" alt="Slider Image 1"></li>
+              </ul>
+          </div>
+      </div>
+      <footer>
+          <div id="footerTop"></div>
+          <div id="footerSFW">Our Smoke, Fire, and Water Division</div>
+          <div id="footerText">
+
+          </div>
+          <div id="footerBottom">
+              <a href="">onsitetechservices.com</a>
+              www.resorationsincberkshires.com
+
+          </div>
+      </footer>
+  </div>
+
+  <?php
+  require_once 'includes/bottom.inc.php';
+  ?>
+
+</body>
+</html>
